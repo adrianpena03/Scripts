09b4b6a56cc8a07c5f1ff616c9f4ab755fb7636f
Adrian Pena <adrianpena@arkham.lan>
2023-11-23 17:37:54 -0500

    added a cypher script

--- Cypher-Script.py
+++ Cypher-Script.py
@@ -1,1 +1,36 @@
-# Test
+# Cyphers / encryption algorithms
+
+# Implement a cypher which converts text to emoji or something.
+# Be able to implement basic cyphers.
+
+
+# dict = {
+#     ":)": "Happy",
+#     ":(": "Sad",
+#     ":O": "Surprised"
+# }
+
+# user_input = input("Look at dictionary, input what you want to receive as output based on input. ")
+# sep = user_input.split(' ')
+
+# for word in sep:
+#     for key, value in dict.items():
+#         if word == key:
+#             print(value, end=' ')
+
+
+# ----------
+# Below is more efficient code
+
+dict = {
+    ":)": "Happy",
+    ":(": "Sad",
+    ":O": "Surprised"
+}
+
+user_input = input("Look at dictionary, input what you want to receive as output based on input. ")
+sep = user_input.split(' ')
+
+for word in sep:
+    print(dict.get(word, word), end=' ')
+
