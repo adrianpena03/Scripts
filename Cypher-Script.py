e7918184a08c17bfc14c55f4ae78c478c6a70faf
Adrian Pena <adrianpena@arkham.lan>
2024-01-01 19:33:01 -0500

    Erased incorrect code

--- Cypher-Script.py
+++ Cypher-Script.py
@@ -2,25 +2,7 @@
 
 # Implement a cypher which converts text to something.
 # Be able to implement basic cyphers.
-
-
-# dict = {
-#     ":)": "Happy",
-#     ":(": "Sad",
-#     ":O": "Surprised"
-# }
-
-# user_input = input("Look at dictionary, input what you want to receive as output based on input. ")
-# sep = user_input.split(' ')
-
-# for word in sep:
-#     for key, value in dict.items():
-#         if word == key:
-#             print(value, end=' ')
-
-
 # ----------
-# Below is more efficient code
 
 dict = {
     ":)": "Happy",
